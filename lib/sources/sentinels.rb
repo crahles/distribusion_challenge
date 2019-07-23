1a25a1f0f8773a910978606ddca896084aeeaaaa
Christoph Rahles <christoph@rahles.de>
2019-07-23 08:56:45 +0200

    add sentinels extraction

--- lib/sources/sentinels.rb
+++ lib/sources/sentinels.rb
@@ -6,6 +6,21 @@ class Sentinels
   end
 
   def extract
-    {}
+
+    routes = @content['sentinels/routes.csv']
+
+    routes.map do |route|
+      next unless node_available route['node']
+      OpenStruct.new(
+        start_node: route['node'],
+        end_node: route['node'],
+        start_time: route['time'],
+        end_time: route['time']
+      )
+    end.compact
+  end
+
+  def node_available(node)
+    %w[alpha beta gamma delta theta lambda tau psi omega].include? node
   end
 end
